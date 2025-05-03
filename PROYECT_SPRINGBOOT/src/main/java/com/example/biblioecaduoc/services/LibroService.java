ad39778f47d4d802d49d6af3b4e616353bf32451
stredes <gianlucassanmartin@gmail.com>
2025-05-03 01:14:46 -0400

    Remueve PROYECT_SPRINGBOOT como submódulo y lo añade como carpeta normal

---
+++ PROYECT_SPRINGBOOT/src/main/java/com/example/biblioecaduoc/services/LibroService.java
@@ -0,0 +1,36 @@
+package com.example.biblioecaduoc.services;
+
+import com.example.biblioecaduoc.model.Libro;
+import com.example.biblioecaduoc.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class LibroService {
+    @Autowired
+    private LibroRepository libroRepository;
+
+    public List <Libro> getLibros(){
+        return libroRepository.obtenerLibros();
+    }
+
+    public Libro saveLibro(Libro libro){
+        return libroRepository.guardar(libro);
+    }
+
+    public Libro getLibroId(int id){
+        return libroRepository.buscarPorId(id);
+    }
+
+    public Libro updateLibro(Libro libro){
+        return libroRepository.actualizar(libro);
+    }
+
+    public String deleteLibro(int id){
+        libroRepository.eliminar(id);
+        return "producto eliminado";
+    }
+    
+
+}
