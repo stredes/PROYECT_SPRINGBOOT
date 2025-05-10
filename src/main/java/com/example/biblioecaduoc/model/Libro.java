bd71b52723b66d637a2986c0d74c1a432e34ae93
stredes <gianlucassanmartin@gmail.com>
2025-05-09 21:19:22 -0400

    resolviendola

--- src/main/java/com/example/biblioecaduoc/model/Libro.java
+++ src/main/java/com/example/biblioecaduoc/model/Libro.java
@@ -1,18 +1,24 @@
 package com.example.biblioecaduoc.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "libros") // opcional, puedes omitir si el nombre coincide
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Libro {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String titulo;
-    private String autor;   
+    private String autor;
     private String editorial;
-    private int fechaPublicacion;
+    private int fechaPublicacion; // puedes cambiar a LocalDate si prefieres
     private String isbn;
 }
